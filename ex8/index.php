f9f049f6bbbebdc8b0dcc3ffbb44e901b3af075f
rama-new <79932197+rama-new@users.noreply.github.com>
2021-03-09 11:18:51 -0300

    Update index.php

--- ex8/index.php
+++ ex8/index.php
@@ -8,6 +8,8 @@ require_once __DIR__.'/Livro.php';
 
 $pessoa =new Pessoa('Rama','M','35');
 $livro = new Livro('Biografia Rama',$pessoa, 1200);
+$pessoa2 = new Pessoa('Pria','F','1.7');
+$livro->setLeitor($pessoa2);
 // ----------------- //
 // Chamadas e testes //
 // ----------------- //
@@ -20,6 +22,9 @@ echo $livro->getAutor()->getNome().PHP_EOL;
 //printando variavel idade da classe autor agregada a classe de livro
 echo $livro->getAutor()->getIdade().PHP_EOL;
 
+//printando a var nome declarada dentro da variavel pessoa leitor do tipo pessoa em livro
+echo $livro->getLeitor()->getNome().PHP_EOL;
+
 //chama metodo contido na classe Livro e com as chamadas da interface Publicaçao
 //necessário haver corpo para classes com chamadas em Publicacao
 var_export($livro->detalhes());
@@ -29,10 +34,11 @@ $livro->folhear(-10);
 var_export($livro->detalhes());
 
 
-//Agregação, se destruir livro, a ligação é fraca e Pessoa continuará existindo;
+//Agregação, se destruir livro, a ligação é fraca
+unset($livro);
 unset($livro);
 var_dump($livro);
 
-//mas a classe pessoa continuará existindo
+//e classes pessoa e pessoa2 continuarão existindo
 var_dump($pessoa);
-
+var_dump($pessoa2);
