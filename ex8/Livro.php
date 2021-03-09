8a5af2fda176f53f097456be3297da6eb0e2db43
rama-new <79932197+rama-new@users.noreply.github.com>
2021-03-09 11:17:47 -0300

    Update Livro.php

--- ex8/Livro.php
+++ ex8/Livro.php
@@ -1,113 +1,44 @@
 <?php
 
-//livro precisa implementar publicacao
-class Livro implements Publicacao
-{
-    private $titulo;
-    private $autor;
-    private $totPaginas;
-    private $pagAtual=0;
-    private $aberto=0;
-    private $leitor;
-
-    public function __construct($titulo,Pessoa $autor,$totPaginas)
-    {
-        $this->titulo=$titulo;
-        $this->autor=$autor;
-        $this->totPaginas=$totPaginas;
-    }
-
-    public function getTitulo(){
-        return $this->titulo;
-    }
-    public function getAutor(){
-        return $this->autor;
-    }
-    public function getTotPaginas(){
-        return $this->totPaginas;
-    }
-    public function getPagAtual(){
-        return $this->pagAtual;
-    }
-    public function getAberto(){
-        return $this->aberto;
-    }
-    public function getLeitor(){
-        return $this->leitor;
-    }
-    
-    public function setLeitor(Pessoa $leitor)
-    {
-        $this->leitor=$leitor;    
-    }
-    
-    public function setFechado(){
-        $this->aberto=0;
-    }
-    public function setAberto(){
-        $this->aberto=1;
-    }
-    public function setPagAtual($pag)
-    {
-        $this->pagAtual=$pag;
-    }
-
-    public function detalhes()
-    {
-        return $this;
-    }
-
-    public function abrir(){
-        $this->setAberto();
-    }
-
-    public function fechar(){
-        $this->setFechado();
-    }
-    public function folhear($pags){
-
-        if($pags>=0){
-            //se a quantidade de pags mais a pos. atual for maior do que o total, vai pro final
-            if($pags+$this->getPagAtual()>=$this->getTotPaginas())
-            {
-                $this->setPagAtual($this->getTotPaginas());
-            }
-            else{
-                //avança uma quantidade $pags de paginas.  
-                $this->setPagAtual($this->getPagAtual()+$pags);
-            }
-        }
-        else{
-            //se a quantidade de paginas a menos for menor do que a pagina inicial, ele fica no começo
-            if($pags+$this->getPagAtual()<0){
-                $this->setPagAtual(0);
-            }
-            else{
-                $this->setPagAtual($this->getPagAtual()+$pags);
-            }
-        }
-    }
-    public function avancarPag(){
-        if($this->getPagAtual()+1>=$this->getTotPaginas())
-        {
-            $this->setPagAtual($this->getTotPaginas());
-        }
-        else{
-            //avança uma pagina.  
-            $this->setPagAtual($this->getPagAtual()+1);
-        }
-    }
-    public function voltarPag(){
-        //fecha o livro se a pagina atual for a primeira e pedir para voltar
-        if($this->getPagAtual()-1<=0)
-        {
-            $this->fechar();
-        }
-        else{
-            //volta uma pagina.  
-            $this->setPagAtual($this->getPagAtual()-1);
-        }
-    }
-}
+require_once __DIR__.'/Pessoa.php';
+require_once __DIR__.'/Publicacao.php';
+require_once __DIR__.'/Livro.php';
 
 
+
+$pessoa =new Pessoa('Rama','M','35');
+$livro = new Livro('Biografia Rama',$pessoa, 1200);
+$pessoa2 = new Pessoa('Pria','F','1.7');
+$livro->setLeitor($pessoa2);
+// ----------------- //
+// Chamadas e testes //
+// ----------------- //
+//printando variavel titulo em livro
+echo $livro->getTitulo().PHP_EOL;
+
+//printando variavel da classe autor agregada a classe livro
+echo $livro->getAutor()->getNome().PHP_EOL;
+
+//printando variavel idade da classe autor agregada a classe de livro
+echo $livro->getAutor()->getIdade().PHP_EOL;
+
+//printando a var nome declarada dentro da variavel pessoa leitor do tipo pessoa em livro
+echo $livro->getLeitor()->getNome().PHP_EOL;
+
+//chama metodo contido na classe Livro e com as chamadas da interface Publicaçao
+//necessário haver corpo para classes com chamadas em Publicacao
+var_export($livro->detalhes());
+$livro->folhear(101);
+var_export($livro->detalhes());
+$livro->folhear(-10);
+var_export($livro->detalhes());
+
+
+//Agregação, se destruir livro, a ligação é fraca
+unset($livro);
+unset($livro);
+var_dump($livro);
+
+//e classes pessoa e pessoa2 continuarão existindo
+var_dump($pessoa);
+var_dump($pessoa2);
